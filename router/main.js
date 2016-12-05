76aba74a89fa7ee4a853ff51eb983ff9ba1a0714
jhson <jhson@encar.com>
2016-12-05 13:34:37 +0900

    chapter restful complete

--- router/main.js
+++ router/main.js
@@ -9,4 +9,93 @@ module.exports = function(app, fs){
     app.get('/about', function(req, res){
         res.render('about.html');
     });
+
+    app.get('/list', function(req, res){
+        fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function(err, data){
+            console.log(data);
+            res.end(data);
+        });
+    });
+
+    app.get('/getUser/:username', function(req, res){
+        fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function(err, data){
+            var users = JSON.parse(data);
+            res.json(users[req.params.username]);
+        });
+    });
+
+    app.post('/addUser/:username', function(req, res){
+        var result = {};
+        var username = req.params.username;
+
+        if(!req.body['password'] || !req.body['name']){
+            result['success'] = 0;
+            result['error'] = 'invalid request';
+            res.json(result);
+            return;
+        }
+
+        fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function(err, data){
+            var users = JSON.parse(data);
+
+            if(users[username]){
+                result['success'] = 0;
+                result['error'] = 'duplicate';
+                res.json(result);
+                return;
+            }
+
+            users[username] = req.body;
+
+            fs.writeFile(__dirname + '/../data/user.json', JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+                result['succes'] = 1;
+                res.json(result);
+            });
+        });
+    });
+
+    app.put('/updateUser/:username', function(req, res){
+        var result = {};
+        var username = req.params.username;
+
+        if(!req.body['password'] || !req.body['name']){
+            result['success'] = 0;
+            result['error'] = 'invalid request';
+            res.json(result);
+            return;
+        }
+
+        fs.readFile(__dirname + '/../data/' + 'user.json', 'utf8', function(err, data){
+            var users = JSON.parse(data);
+
+            users[username] = req.body;
+
+            fs.writeFile(__dirname + '/../data/user.json', JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+                result['success'] = 1;
+                res.json(result);
+            });
+        });
+    });
+
+    app.delete('/deleteUser/:username', function(req, res){
+        var result = {};
+
+        fs.readFile(__dirname + '/../data/user.json', 'utf8', function(err, data){
+            var users = JSON.parse(data);
+
+            if(!users[req.params.username]){
+                result['success'] = 0;
+                result['error'] = 'NOT FOUND';
+                res.json(result);
+                return;
+            }
+
+            delete users[req.params.username];
+
+            fs.writeFile(__dirname + '/../data/user.json', JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+                result['success'] = 1;
+                res.json(result);
+            });
+        });
+    });
 };
