e91f071b461e1f7b6934da5d0f394549ce5c4d63
jhson <jhson@encar.com>
2016-12-05 17:13:37 +0900

    session complete

--- router/main.js
+++ router/main.js
@@ -1,8 +1,12 @@
 module.exports = function(app, fs){
     app.get('/', function(req, res){
+        var sess = req.session;
+
         res.render('index', {
             title: 'MY HOMPAGE',
-            length: 5
+            length: 5,
+            username: sess.username,
+            name: sess.name
         });
     });
 
@@ -97,5 +101,51 @@ module.exports = function(app, fs){
                 res.json(result);
             });
         });
+    });
+
+    app.get('/login/:username/:password', function(req, res){
+        var sess;
+        sess = req.session;
+
+        fs.readFile(__dirname + '/../data/user.json', 'utf8', function(err, data){
+            var users = JSON.parse(data);
+            var username = req.params.username;
+            var password = req.params.password;
+            var result = {};
+
+            if(!users[username]){
+                result['success'] = 0;
+                result['error'] = 'NOT FOUND';
+                res.json(result);
+                return;
+            }
+
+            if(users[username]['password'] == password){
+                result['success'] = 1;
+                sess.username = username;
+                sess.name = users[username]['name'];
+                res.json(result);
+            }else{
+                result['success'] = 0;
+                result['error'] = 'incorrect';
+                res.json(result);
+            }
+        });
+    });
+
+    app.get('/logout', function(req, res){
+        sess = req.session;
+
+        if(sess.username){
+            req.session.destroy(function(err){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/');
+                }
+            });
+        }else{
+            res.redirect('/');
+        }
     });
 };
